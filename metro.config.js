ff2c3c424b23caa921fb7698fba88a87bc9f1fc0
EMMA <obazeemmmanuel2@gmail.com>
2026-02-25 19:44:58 +0100

    Prefer browser exports in Metro

--- metro.config.js
+++ metro.config.js
@@ -7,4 +7,9 @@ config.resolver.extraNodeModules = {
   crypto: require.resolve("react-native-quick-crypto"),
 };
 
+// Prefer react-native/browser exports to avoid Node-only modules (e.g. jose/zlib).
+config.resolver.mainFields = ["react-native", "browser", "main"];
+config.resolver.unstable_enablePackageExports = true;
+config.resolver.unstable_conditionNames = ["react-native", "browser", "default"];
+
 module.exports = config;
